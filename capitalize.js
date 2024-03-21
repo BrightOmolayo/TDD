a948ed17c131fb76e5c5418ebc8d50b50a054df9
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-03-21 18:46:38 +0100

    calculator

--- capitalize.js
+++ capitalize.js
@@ -2,8 +2,3 @@ function capitalizeFirstChar (str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
 module.exports = capitalizeFirstChar
-
-function reverseString (string) {
-  return string.split('').reverse().join('')
-}
-module.exports = reverseString
--- index.test.js
+++ index.test.js
@@ -1,13 +1,19 @@
-const capitalizeFirstChar = require('./index')
-const reverseString = require('./index')
+const capitalizeFirstChar = require('./capitalize')
+const reverseString = require('./reverse')
+const calculator = require('./calculator')
 
 // eslint-disable-next-line no-undef
 test('capitilize', () => {
   // eslint-disable-next-line no-undef
-  expect(capitalizeFirstChar('thgirB')).toBe('Bright')
+  expect(capitalizeFirstChar('bright')).toBe('Bright')
 })
 // eslint-disable-next-line no-undef
-test('reverseString 1', () => {
+test('reverseString', () => {
   // eslint-disable-next-line no-undef
   expect(reverseString('olleh')).toBe('hello')
+})
+// eslint-disable-next-line no-undef
+test('calculator', () => {
+  // eslint-disable-next-line no-undef
+  expect((2 * 2)).toBe(4)
 })
